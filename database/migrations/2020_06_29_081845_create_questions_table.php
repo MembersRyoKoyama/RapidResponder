61b60a016a1cf14662b22719873811f2969c6a79
mmmngit <tourokuyouf@gmail.com>
2020-06-30 17:27:14 +0900

    create migration file

---
+++ database/migrations/2020_06_29_081845_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('mail');
+            $table->string('tel');
+            $table->foreignId('products_id')->constrained();
+            $table->text('content');
+            $table->tinyInteger('end');
+            $table->foreignId('staffs_id')->nullable()->constrained('users');
+            $table->timestamp('date')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
