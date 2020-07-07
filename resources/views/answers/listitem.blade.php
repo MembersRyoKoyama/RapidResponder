5b708154a5036c316dc8df0a3313c7f19e3ebdc8
root <kymr.1010@gmail.com>
2020-07-07 02:17:27 +0000

    pull

--- resources/views/answers/listitem.blade.php
+++ resources/views/answers/listitem.blade.php
@@ -1,14 +1,13 @@
 <tbody>
-  <td>
-    @include('answers.endIcon',['end'=>$q->end])
-    @if($q->end!=1)
-    {{$q->staffs->name??'名無し'}}
-    @endif
-  </td>
-  <td>{{$q->name}}</td>
-  <td>{{$q->tel}}</td>
-  <td>{{$q->products->name}}</td>
-  <td>{{$q->date}}</td>
-  <td>{{Str::substr($q->content,0,100)}}</td>
-  <td><a href="/questionView?id={{$q->id}}">詳細</a></td>
+  <tr class="d-flex">
+    <td class="col-1" scope="row">
+      @include('answers.endIcon',['end'=>$q->end])
+    </td>
+    <td class="col-1">{{$q->name}}</td>
+    <td class="col-2">{{$q->tel}}</td>
+    <td class="col-1">{{$q->products->name}}</td>
+    <td class="col-2">{{$q->date}}</td>
+    <td class="col-4">{{Str::substr($q->content,0,100)}}</td>
+    <td class="col-1"><a href="/questionView?id={{$q->id}}">詳細</a></td>
+  </tr>
 </tbody>
