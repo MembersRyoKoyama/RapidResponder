ae570787c10236d4aef55b125c5f0ceffb53fc13
root <kymr.1010@gmail.com>
2020-07-07 02:11:13 +0000

    a

--- resources/views/answers/listitem.blade.php
+++ resources/views/answers/listitem.blade.php
@@ -1,6 +1,9 @@
 <tbody>
   <td>
     @include('answers.endIcon',['end'=>$q->end])
+    @if($q->end!=1)
+    {{$q->staffs->name??'名無し'}}
+    @endif
   </td>
   <td>{{$q->name}}</td>
   <td>{{$q->tel}}</td>
