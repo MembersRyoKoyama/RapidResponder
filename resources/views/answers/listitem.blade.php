ba9c7e79eea64be01023b49692e5413038916a70
root <kymr.1010@gmail.com>
2020-07-07 02:30:31 +0000

    make visible staff in list

--- resources/views/answers/listitem.blade.php
+++ resources/views/answers/listitem.blade.php
@@ -1,7 +1,10 @@
 <tbody>
   <tr class="d-flex">
     <td class="col-1" scope="row">
-      @include('answers.endIcon',['end'=>$q->end])
+      @include('answers.endIcon',['end'=>$q->end])<br>
+      @if($q->end!=1)
+      {{$question->users==null?'名無し':$question->users->name}}
+      @endif
     </td>
     <td class="col-1">{{$q->name}}</td>
     <td class="col-2">{{$q->tel}}</td>
